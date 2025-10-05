f2e6e9825c0d927102be194008b669ff482460c1
Vadim Sukhomlynov <97035738+Vadim-Sukhomlinov@users.noreply.github.com>
2025-10-05 14:36:51 +0300

    Add GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
+}
+
+module.exports = nextConfig
